c67e84860cba1bcd813eb3d19a4f1ffbc61fcaa2
Semyon Savchenko <savhenko10@mail.ru>
2022-12-17 17:38:47 +0700

    blyat'

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import scipy as sci
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
+from pmdarima import auto_arima
 from statsmodels.graphics import tsaplots
 from statsmodels.graphics.tsaplots import plot_pacf
 from statsmodels.tsa.arima.model import ARIMA
@@ -12,13 +13,13 @@ from plotly.offline import init_notebook_mode, plot
 from plotly import graph_objs as go
 from IPython.display import display
 from sklearn.metrics import mean_absolute_error, mean_absolute_percentage_error, mean_squared_error
-from statsmodels.tsa.holtwinters import Holt, ExponentialSmoothing
+from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 init_notebook_mode(connected=True)
 warnings.simplefilter("ignore")
 
 
-def plotly_df(df, title = ''):
+def plotly_df(df, title=''):
     data = []
     for column in df.columns:
         trace = go.Scatter(
@@ -33,9 +34,11 @@ def plotly_df(df, title = ''):
     fig = dict(data=data, layout=layout)
     plot(fig, show_link=False)
 
+
+i = 48
 #полученние данных(цены акции) из сsv файла
 data_set = pd.read_csv('//Users//semensavcenko//Downloads//ll.csv')
-msk = (data_set.index < len(data_set) - 35)
+msk = (data_set.index < len(data_set) - i)
 data_set_train = data_set[msk].copy()
 data_set_test = data_set[~msk].copy()
 
@@ -50,25 +53,26 @@ print(data_set_train.isnull().sum())
 data_set.dropna()
 
 #рисуем гистограмму
-plt.hist(data_set_train, 20, facecolor='purple')
-plt.show()
+#plt.hist(data_set_train, 20, facecolor='purple')
+#plt.show()
 
 #рисуем коррелограмму с лагом от 1 до 60
-tsaplots.plot_acf(data_set_train, lags=60, color='g')
+tsaplots.plot_acf(data_set_train, lags=20, color='g')
 plt.show()
 
 #рисуем ЧАКФ без указания метода коэф будет выходить за границы интервала [-1;1]
-plot_pacf(data_set, lags=60, method='ywmle', color='purple')
+plot_pacf(data_set, lags=20, method='ywmle', color='purple')
 plt.show()
 
 #проверим ряд на стационарность при помощи теста Дукки-Фуллера
 #H0: Временной ряд является нестационарным.
 print("Проверка на стационарность - \n")
 test = sm.tsa.adfuller(data_set_train)
+print ('adf: ', test[0])
 print("p-value: ", test[1])
 print("Critical values: ", test[4]['5%'])
 
-if test[1] > test[4]['5%']:
+if test[0] > test[4]['5%']:
     print('H0 не отвергается')
 else:
     print('H0 отверагется')
@@ -118,6 +122,7 @@ def Foster_Stuart_test(X, p_level=0.95):
 
     return result
 
+
 result = Foster_Stuart_test(data_set_train)
 pd.set_option('display.max_columns', None)
 print("Проверка на тенеденцию - \n")
@@ -129,21 +134,44 @@ display(result)
 #I - Integrated помогает нам сделать ряд стационарным
 #MA - Moving Average означает что временной ряд может быть
 #регрессирован на прошлых ошибках
+otg1diff = data_set_train.diff(periods=1).dropna()
+
+test1 = sm.tsa.adfuller(otg1diff)
+print("adf: ", test1[0])
+print("p-value: ", test1[1])
+print("Critical values: ", test1[4]['5%'])
+
+if test1[0] > test1[4]['5%']:
+    print('H0 не отвергается')
+else:
+    print('H0 отверагется')
+plot_pacf(otg1diff, lags=20, title='PACF for AR')
+tsaplots.plot_acf(otg1diff, lags=20, title='ACF for MA')
+
 print("\n")
 print("\t\t\t\t\t\t\t\tARIMA")
-model = ARIMA(data_set_train, order=(1, 1, 1)).fit()
+model = ARIMA(data_set_train, order=(1, 1, 1), seasonal_order=(0, 0, 0, 0), trend='t').fit()
 print(model.summary())
 #оценка значимости модели
 
 #адекватность модели
 myresiduals = pd.DataFrame(model.resid)
-fig, ax = plt.subplots(1, 2)
+_, ax = plt.subplots(1, 2)
 myresiduals.plot(title="Residuals", ax=ax[0])
 myresiduals.plot(kind='kde', title='Density', ax=ax[1])
-plt.show()
 
+
+_, axes = plt.subplots(2, 2, sharex=True)
+axes[0, 0].plot(data_set_train)
+axes[0, 0].set_title('The Genuine Series')
+tsaplots.plot_acf(data_set_train, ax=axes[0, 1])
+# Order of Differencing: First
+axes[1, 0].plot(data_set_train.diff())
+axes[1, 0].set_title('Order of Differencing: First')
+tsaplots.plot_acf(data_set_train.diff().dropna(), ax=axes[1, 1])
 #прогноз
-forecast = model.forecast(35)
+forecast = model.forecast(i)
+print(len(data_set_train))
 data_set['forecast'] = [None] * len(data_set_train) + list(forecast)
 data_set.plot()
 plt.show()
@@ -158,33 +186,33 @@ print('mae: ', mae)
 
 #средняя абсолютная ошибка в %
 mape = mean_absolute_percentage_error(data_set_test, forecast)
-print(f'mape: {mape}%')
+print(f'mape: {mape*100}%')
 print("END ARIMA")
-#по моим данных следующие значения
-#mse:  1636.8611498373427
-#mae:  1404.5483420297503
-#mape: 0.30054720137066887%
-#Величина MAPE подсчитанная программой получилась 0,3 %.
-#Имея данные, не использованные в прогнозе,
-#мы получили MAPE, равную 0,3%.
-#Значит, точность прогноза составляет – 99,7%.
-#Такая точность прогноза является хорошей
 
+
+model_auto_arima = auto_arima(data_set_train, start_p=0, d=1, start_q=0)
+print(model_auto_arima)
+pred = model_auto_arima.predict(n_periods=i)
+plt.plot(data_set_test)
+plt.plot(pred)
+plt.show()
+mape = mean_absolute_percentage_error(data_set_test, pred)
+print(f'mape: {mape*100}%')
 #для отчета
 #print(data_set_test.tail(35))
 
 
-#прогнозирование на основе модели Двойного линейного сглаживания, т.к. он учитывает тендецию, в отличие от модели 1 порядка
-model2 = ExponentialSmoothing(np.asarray(data_set_train)).fit()
-forecast2 = model2.forecast(35)
+#прогнозирование на основе модели Двойного exp сглаживания, т.к. он учитывает тендецию, в отличие от модели 1 порядка
+model2 = ExponentialSmoothing(np.asarray(data_set_train), trend='add', seasonal=None, damped_trend=True).fit()
+forecast2 = model2.forecast(i)
+print(model2.params)
 data_set['forecast'] = ''
 data_set['forecast2'] = [None] * len(data_set_train) + list(forecast2)
 data_set.plot()
 plt.show()
-
 #адекватность модели
 myres = pd.DataFrame(model2.resid)
-fig, ax = plt.subplots(1, 2)
+_, ax = plt.subplots(1, 2)
 myres.plot(title="Residuals", ax=ax[0])
 myres.plot(kind='kde', title='Density', ax=ax[1])
 plt.show()
@@ -200,15 +228,5 @@ print('mae: ', mae)
 
 #средняя абсолютная ошибка в %
 mape = mean_absolute_percentage_error(data_set_test, forecast2)
-print(f'mape: {mape}%')
+print(f'mape: {mape * 100}%')
 print("END LIN EXP")
-
-#по моим данных следующие значения
-#mse:  1547.4019397275374
-#mae:  1320.924408869623
-#mape: 0.2829626590338698%
-#Величина MAPE подсчитанная программой получилась 0,28 %.
-#Имея данные, не использованные в прогнозе,
-#мы получили MAPE, равную 0,28%.
-#Значит, точность прогноза составляет – 99,72%.
-#Такая точность прогноза является хорошей
